b16b047bc55e3743dead112ba32adfdcf1b14d5f
samiunique <yitesam@gmail.com>
2024-02-02 04:41:52 -0800

    betty style insured white space removed add dos ending fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function that creates a binary tree node.
+ * binary_tree_node - function that creates a binary tree node.i
  *@parent: is a pointer to the parent node of the node to create.
  *@value: is the value to put in the new node.
  * Return:Your function must return a pointer to the new node or NULL if no.
@@ -12,7 +12,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (node == NULL)
 		return (NULL);
-	/*node->n = value;*/
 	(*node).n = value;
 	/*node->parent = parent;*/
 	(*node).parent = parent;
